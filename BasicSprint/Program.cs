00883104bd18c3f4e3f6e4ab5e1b2a50417559d5
Yosefgid <jossykasshun@yahoo.com>
2026-03-11 12:05:24 +0000

    Added a method for greeting and Console check

--- BasicSprint/Program.cs
+++ BasicSprint/Program.cs
@@ -5,13 +5,17 @@
         static void Main(string[] args)
         {
             double pi = 3.14;
-            Console.WriteLine(pi);
             bool isLearningCSharpFun = true;
-            Console.WriteLine(isLearningCSharpFun);
             string favouriteFilm = "Avatar";
-            Console.WriteLine("My Favourite films is " + favouriteFilm);
             favouriteFilm = "High School Musical";
-            Console.WriteLine(favouriteFilm);
+            string firstName = "Yosef";
+            printGreeting(firstName);
         }
+
+        private static void printGreeting(string name)
+        {
+            Console.WriteLine("Hello, Good Morning " + name);
+        }
+
     }
 }
